616a99764515a1a2f51e7e8170eb2c4a338b9338
Mamadou Lamine DIAKHABY <mamadou.lamine.diakhaby20@gmail.com>
2022-12-09 16:36:22 +0100

    mettre à jour le script blog.sh

--- blog.sh
+++ blog.sh
@@ -19,5 +19,101 @@ case $# in
             echo "File help.sh not found"
           exit 0
           fi
+    elif [ "$1" = "addImg" ]
+       then
+          echo "veuillez indiquer le chemin de l'image"
+    elif [ "$1" = "addArticle" ]
+       then
+          echo "il manque un argument! "
+    elif [ "$1" = "build" ]
+       then
+          echo "veuillez indiquer le chemin du dossier "
+     else
+       echo  "Syntax error"
      fi;;
+  2) 
+
+    if [ "$1" = "addImg" ]
+      then
+
+        "$2" "images/"
+
+        IMG_PATH=$2
+        IMGDIR_PATH="images/"
+        export IMG_PATH
+        export IMGDIR_PATH
+
+       if [ -e "fichiers/scripts/addImage.sh" ];then
+        /bin/bash fichiers/scripts/addImage.sh
+       else
+         echo "Scrpit addImage.sh not found"
+       exit 0
+       fi 
+
+    elif [ "$1" = "addArticle" ]
+      then
+
+        "$2"
+
+        ARTICLE_PATH=$2
+        export ARTICLE_PATH
+
+        if [ -e "fichiers/scripts/addArticle.sh" ];then
+        /bin/bash fichiers/scripts/addArticle.sh
+       else
+         echo "Script addArticle.sh not found"
+       exit 0
+       fi 
+    
+    elif [ "$1" = "build" ]
+      then
+
+        #build "$2"
+
+        BUILD_PATH=$2
+        export BUILD_PATH
+
+        if [ -e "fichiers/scripts/build.sh" ];then
+        /bin/bash fichiers/scripts/build.sh
+       else
+         echo "script build.sh not found"
+       exit 0
+       fi
+
+
+    else
+       echo "Syntax error"
+     fi;;  
+  3)
+    if [ "$1" = "addArticle"  ] && [ "$3" = "addImg"  ]
+      then
+         echo "Il manque un argument."
+    else
+    echo "Syntax error."
+    fi;;
+  4)
+    
+    if [ "$1" = "addArticle" ] && [ "$3" = "addImg"  ]
+      then
+        "$2" "$4" "fichiers/articles/images/"
+
+        ARTICLE_PATH=$2
+        IMGDIR_PATH="fichiers/articles/images/"
+        IMG_PATH=$4
+        export ARTICLE_PATH
+        export IMGDIR_PATH
+        export IMG_PATH
+
+        if [ -e "fichiers/scripts/addArticleWithImage.sh" ];then
+        /bin/bash fichiers/scripts/addArticleWithImage.sh
+       else
+         echo "scripts addArticleWithImage.sh not found"
+       exit 0
+       fi 
+
+    else
+      echo "Syntax error."
+    fi;;
+  *)
+    echo "Beaucoup trop d'arguments !!!";;
 esac
